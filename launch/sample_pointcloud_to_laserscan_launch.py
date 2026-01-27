9a611d40427c0e6c9f8841f1e20689b01cb7e2b6
namanmore <naman.more50.nm@gmail.com>
2026-01-27 11:24:39 +0530

    Fixed topic name

--- launch/sample_pointcloud_to_laserscan_launch.py
+++ launch/sample_pointcloud_to_laserscan_launch.py
@@ -6,13 +6,10 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        DeclareLaunchArgument(
-            name='scanner', default_value='scanner',
-            description='Namespace for sample topics'
-        ),
+
         Node(
             package='pointcloud_to_laserscan', executable='dummy_pointcloud_publisher',
-            remappings=[('cloud', [LaunchConfiguration(variable_name='scanner'), '/cloud'])],
+            remappings=[('cloud', '/cloud')],
             parameters=[{'cloud_frame_id': 'cloud', 'cloud_extent': 2.0, 'cloud_size': 500}],
             name='cloud_publisher'
         ),
@@ -24,8 +21,8 @@ def generate_launch_description():
         ),
         Node(
             package='pointcloud_to_laserscan', executable='pointcloud_to_laserscan_node',
-            remappings=[('cloud_in', [LaunchConfiguration(variable_name='scanner'), '/cloud']),
-                        ('scan', [LaunchConfiguration(variable_name='scanner'), '/scan'])],
+            remappings=[('cloud_in', '/cloud'),
+                        ('scan', '/scan')],
             parameters=[{
                 'target_frame': 'cloud',
                 'transform_tolerance': 0.01,
